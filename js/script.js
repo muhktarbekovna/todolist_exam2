f944e548995d3f3f3efb6d61d1412d33979acf99
muhktarbekovna <muhktarbekovna@gmail.com>
2022-11-29 13:58:26 +0600

    add script

--- js/script.js
+++ js/script.js
@@ -5,11 +5,20 @@ const btn = document.querySelector('.add-btn')
 const ul = document.querySelector('.list-group-item')
 
 btn.addEventListener("click" , () => {
+    addToDo()
+})
+input_text && surname_input && tell_input.addEventListener("keydown",(e) => {
+    if (e.key === "Enter"){
+        addToDo()
+    }
+})
+
+function addToDo(){
     if (input_text.value !== "" && tell_input.value !== "" && surname_input.value !== ""){
         const newList = `<li class="list-group-item d-flex justify-content-between align-items-center">
           
           <span class="d-flex align-items-center">
-          <div class="circle">${input_text.value[0].toUpperCase()} ${surname_input.value[0].toUpperCase()}</div>
+          <div class="circle" style="background: ${randomColor()}">  ${input_text.value[0].toUpperCase()} ${surname_input.value[0].toUpperCase()}</div>
           <input type="checkbox" class="mx-2 checkbox">
           ${"Имя:"}${input_text.value} <br> ${"Фамилия:"} ${surname_input.value} <br>  ${"тел:"}${tell_input.value}
           </span>
@@ -17,13 +26,32 @@ btn.addEventListener("click" , () => {
           </li>`
         ul.innerHTML += newList
     }
-})
+    input_text.value = ""
+    surname_input.value = ""
+    tell_input.value = ""
+}
+
+function randomColor(){
+    let result = ""
+    for (let i = 0; i < 6 ; i++){
+        let randomNum = Math.floor((Math.random()*10))
+        result +=  randomNum
+    }
+    return "#" + result
+}
+
+// console.log(randomColor())
+
 
 ul.addEventListener("click", (e) => {
     if (e.target.classList.contains("del-btn")){
         e.target.parentNode.remove()
     }
     if (e.target.classList.contains("checkbox")) {
-        e.target.parentNode.classList.toggle("line")
+        if(!e.target.parentNode.classList.contains("line")) {
+            e.target.parentNode.classList.add("line")
+        }else {
+            e.target.parentNode.classList.remove("line")
+        }
     }
 })
